62c412db036fde57b830c0be8716f4e0194bc8cf
Ozenkol <108022190+Ozenkol@users.noreply.github.com>
2023-01-18 21:57:20 +0600

    First commit

---
+++ tsis1/python_comments.py
@@ -0,0 +1,11 @@
+#This is a comment
+print("Hello, World!")
+
+#####################
+
+print("Hello, World!") #This is a comment
+
+#####################
+
+#print("Hello, World!")
+print("Cheers, Mate!")
