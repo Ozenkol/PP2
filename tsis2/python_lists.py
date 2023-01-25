b96229fc5cedaa7327da619b3bcbd45d55e8ce0e
Temirlan Tashen <t_tashen@kbtu.kz>
2023-01-25 15:13:11 +0600

    tsis 2 in KBTU computer

---
+++ tsis2/python_lists.py
@@ -0,0 +1,34 @@
+mylist = ["apple", "banana", "cherry"]
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+print()
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+print()
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+print()
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print()
+
+list1 = ["abc", 34, True, 40, "male"]
+
+print()
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+print()
+
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
---
+++ tsis2/python_tuples.py
@@ -0,0 +1,43 @@
+mytuple = ("apple", "banana", "cherry")
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+print()
+
+thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+print(thistuple)
+
+print()
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+print()
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+print()
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+print()
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+tuple1 = ("abc", 34, True, 40, "male")
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+print()
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
+
+
