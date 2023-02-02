232fb0cc6bfc7875cfb2e143178349d91769a0eb
Ozenkol <108022190+Ozenkol@users.noreply.github.com>
2023-02-02 21:22:40 +0600

    Updated_6

---
+++ tsis2/python_tuples/loop_tuples.py
@@ -0,0 +1,20 @@
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+
+print()
+
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+
+print()
+
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
+
+print()
+
---
+++ tsis2/python_tuples/tuple_exercises.py
@@ -0,0 +1,17 @@
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+
+print()
+
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+
+print()
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+
+print()
+
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
