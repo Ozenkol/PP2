7c269405166c710306735e9fbefdb5b2caddc247
Ozenkol <108022190+Ozenkol@users.noreply.github.com>
2023-02-26 00:21:03 +0600

    Updated_23

--- tsis5/regex/1.py
+++ tsis5/regex/1.py
@@ -4,6 +4,10 @@ with open("raw.txt", "r", encoding="utf8") as file:
     data = file.read()
 
 pattern = '\w+а{1}б*\w+'
-for word in re.finditer(pattern, data):
-    print("Word: ", word[0])
+
+print(re.findall(pattern, data))
+
+#Для проверки
+#for word in re.finditer(pattern, data):
+#    print("Word: ", word[0])
 
